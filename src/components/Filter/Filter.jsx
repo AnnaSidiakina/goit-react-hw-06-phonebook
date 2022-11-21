71b6fc913a72a149210fdeac853c25d9c32e6cc0
AnnaSidiakina <102148925+AnnaSidiakina@users.noreply.github.com>
2022-11-21 10:15:33 +0100

    Update Filter.jsx
    fix

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import styles from './Filter.module.css';
 import { filterContacts } from '../../redux/contactsSlice';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Filter = () => {
   const dispatch = useDispatch();
+  const filter = useSelector(state => state.contacts.filter.value);
   return (
     <div className={styles.form}>
       <label className={styles.label}>
         Filter
         <input
+          value={filter}
           className={styles.input}
           type="text"
           onChange={e => dispatch(filterContacts(e.currentTarget.value))}
